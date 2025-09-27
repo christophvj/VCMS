9f231623d43edd89ba52039f06fe10025c9b7db5
Christoph <155946732+christophvj@users.noreply.github.com>
2025-09-27 14:51:24 +0200

    Added Browse and register for events page. Not done. Creating a pull to catch up with new code to prevent merge conflicts

---
+++ VCMS/Browse_Register_Event.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Browse_Register_Event.aspx.cs" Inherits="VCMS.Browse_Register_Event" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <!-- Div "PageHeading" Contains Label for Page Title -->
+        <div class ="PageHeading">
+            <asp:Label ID="lblPageHeading" runat="server" Text="Register For Events"></asp:Label>
+        </div>
+
+        <div class="EventBlocks">
+            <asp:Repeater ID="rptEvents" runat="server" OnItemCommand="rptEvents_ItemCommand" OnItemDataBound="rptEvents_ItemDataBound">
+                    <ItemTemplate>
+                        <div class="EventCard">
+
+                            <h3><%# Eval("EventName") %></h3>
+                            <p><strong>Beneficiaries:</strong> <%# Eval("Beneficiaries") %></p>
+                            <p><strong>Description:</strong> <%# Eval("Description") %></p>
+                            <p><strong>Location:</strong> <%# Eval("Location") %></p>
+                            <p><strong>Start Date:</strong> <%# Eval("StartDate", "{0:MMMM dd, yyyy}") %></p>
+                            <p><strong>End Date:</strong> <%# Eval("EndDate", "{0:MMMM dd, yyyy}") %></p>
+
+                            <asp:DropDownList ID="ddlSkills" runat="server"></asp:DropDownList>
+                            <br /><br />
+
+                            <asp:Button ID="btnRegister" runat="server" Text="Register" CommandName="Register" CommandArgument='<%# Eval("EventID") %>' />
+
+                        </div>
+                    </ItemTemplate>
+            </asp:Repeater>
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ VCMS/Browse_Register_Event.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VCMS
+{
+    public partial class Browse_Register_Event : System.Web.UI.Page
+    {
+
+            DataBaseControls db = new DataBaseControls();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindEvents();
+            }
+        }
+
+        private void BindEvents()
+        {
+            // Get all events from the DatabaseControls Class
+           // rptEvents.DataSource = db.GetAllEventsWithBeneficiaries();
+            rptEvents.DataBind();
+        }
+
+        protected void rptEvents_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                DropDownList ddlSkills = (DropDownList)e.Item.FindControl("ddlSkills");
+               // ddlSkills.DataSource = db.GetSKills();
+                ddlSkills.DataTextField = "Description";
+                ddlSkills.DataValueField = "SkillID";
+                ddlSkills.DataBind();
+            }
+        }
+
+        // Handle register button click event
+        protected void rptEvents_ItemDataBound(object source, RepeaterItemEventArgs e)
+        {
+            //if (e.CommandName == "Register")
+            {
+                // Ensure user is loged in 
+                if (Session["UserID"] == null)
+                {
+                    Response.Redirect("Login.aspx");
+                    return;
+                }
+
+                int userID = Convert.ToInt32(Session["UserID"]);
+                //int eventID = Convert.ToInt32(e.CommandArgument);
+
+                DropDownList ddlSkills = (DropDownList)e.Item.FindControl("ddlSkills");
+                int skillID = Convert.ToInt32(ddlSkills.SelectedValue);
+
+                // Register the user for the event
+               // db.RegisterUserForEvent(userID, eventID, skillID);
+
+                Response.Write("<script>alert('Successfully registered as a volunteer!');</script>");
+            }
+        }
+    }
+}
