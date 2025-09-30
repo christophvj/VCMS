8a86a1bb410cc836679afda32abdc12b946ec93e
Christoph Van Jaarsveld <christophvj08@gmail.com>
2025-09-30 22:28:02 +0200

    Handed out work to team. Created pages for their work

---
+++ VCMS/EventParticipation.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VCMS
+{
+    public partial class EventParticipation : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Create instance for Connection String 
+        DataBaseControls db = new DataBaseControls();
+        // Example. db.connectionString;
+        // User Id is stored in Session after login Session["UserID"] = userId;
+    }
+}
---
+++ VCMS/UpdateUserInfo.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VCMS
+{
+    public partial class UpdateUserInfo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        // Use connection string from DataBaseControls
+        // Example: DataBaseControls db = new DataBaseControls();
+        // db.connectionString;
+    }
+}
