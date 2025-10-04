164d678c93c1790f2590702a24c4764301710ea4
Rieghardt Wehmeyer <rieghardtwehmeyer@outlook.com>
2025-10-04 11:29:49 +0200

    Added the "Manage-users-and-benificiaries" code

--- VCMS/ManageUsersAndBeneficiaries.aspx
+++ VCMS/ManageUsersAndBeneficiaries.aspx
@@ -1,14 +1,48 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ManageUsersAndBeneficiaries.aspx.cs" Inherits="VCMS.ManageUsersAndBeneficiaries" %>
 
 <!DOCTYPE html>
-
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>Manage Users and Beneficiaries</title>
 </head>
 <body>
     <form id="form1" runat="server">
-        <div>
+        <div style="margin:20px;">
+            <h2>Add New User</h2>
+            <asp:TextBox ID="txtName" runat="server" Placeholder="Name"></asp:TextBox><br />
+            <asp:TextBox ID="txtSurname" runat="server" Placeholder="Surname"></asp:TextBox><br />
+            <asp:TextBox ID="txtEmail" runat="server" Placeholder="Email"></asp:TextBox><br />
+            <asp:TextBox ID="txtPhoneNumber" runat="server" Placeholder="Phone Number"></asp:TextBox><br />
+            <asp:TextBox ID="txtPassword" runat="server" TextMode="Password" Placeholder="Password"></asp:TextBox><br />
+            <asp:Button ID="btnAddUser" runat="server" Text="Add User" OnClick="btnAddUser_Click" />
+
+            <h3>Remove Users</h3>
+            <asp:GridView ID="gvUsers" runat="server" AutoGenerateColumns="False" DataKeyNames="UserID" OnRowDeleting="gvUsers_RowDeleting">
+                <Columns>
+                    <asp:BoundField DataField="UserID" HeaderText="UserID" ReadOnly="True" />
+                    <asp:BoundField DataField="Name" HeaderText="Name" />
+                    <asp:BoundField DataField="Surname" HeaderText="Surname" />
+                    <asp:BoundField DataField="Email" HeaderText="Email" />
+                    <asp:CommandField ShowDeleteButton="True" />
+                </Columns>
+            </asp:GridView>
+
+            <hr />
+
+            <h2>Add New Beneficiary</h2>
+            <asp:TextBox ID="txtBeneficiaryName" runat="server" Placeholder="Name"></asp:TextBox><br />
+            <asp:TextBox ID="txtAmountReceived" runat="server" Placeholder="Amount Received"></asp:TextBox><br />
+            <asp:Button ID="btnAddBeneficiary" runat="server" Text="Add Beneficiary" OnClick="btnAddBeneficiary_Click" />
+
+            <h3>Remove Beneficiaries</h3>
+            <asp:GridView ID="gvBeneficiaries" runat="server" AutoGenerateColumns="False" DataKeyNames="BeneficiaryID" OnRowDeleting="gvBeneficiaries_RowDeleting">
+                <Columns>
+                    <asp:BoundField DataField="BeneficiaryID" HeaderText="BeneficiaryID" ReadOnly="True" />
+                    <asp:BoundField DataField="Name" HeaderText="Name" />
+                    <asp:BoundField DataField="AmountReceived" HeaderText="Amount Received" />
+                    <asp:CommandField ShowDeleteButton="True" />
+                </Columns>
+            </asp:GridView>
         </div>
     </form>
 </body>
--- VCMS/ManageUsersAndBeneficiaries.aspx.cs
+++ VCMS/ManageUsersAndBeneficiaries.aspx.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Data;
+using System.Data.SqlClient;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -9,12 +8,119 @@ namespace VCMS
 {
     public partial class ManageUsersAndBeneficiaries : System.Web.UI.Page
     {
+        //Connection string
+        string connectionString = @"Server=DESKTOP-4K2005S;Database=VCMS_DB;Trusted_Connection=True;";
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                LoadUsers();
+                LoadBeneficiaries();
+            }
         }
-        // Use connection string from DataBaseControls
-        // Example: DataBaseControls db = new DataBaseControls();
-        // db.connectionString;
+
+        //Load Users into GridView
+        private void LoadUsers()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT UserID, Name, Surname, Email FROM Users", con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                gvUsers.DataSource = dt;
+                gvUsers.DataBind();
+            }
+        }
+
+        //Load Beneficiaries into GridView
+        private void LoadBeneficiaries()
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT BeneficiaryID, Name, AmountReceived FROM Beneficiary", con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                gvBeneficiaries.DataSource = dt;
+                gvBeneficiaries.DataBind();
+            }
+        }
+
+        //Add New User
+        protected void btnAddUser_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Users (Name, Surname, Email, PhoneNumber, Password) VALUES (@Name, @Surname, @Email, @PhoneNumber, @Password)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                cmd.Parameters.AddWithValue("@Surname", txtSurname.Text);
+                cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                cmd.Parameters.AddWithValue("@PhoneNumber", txtPhoneNumber.Text);
+                cmd.Parameters.AddWithValue("@Password", txtPassword.Text);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+
+            LoadUsers();
+        }
+
+        //Delete User from GridView
+        protected void gvUsers_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int userId = Convert.ToInt32(gvUsers.DataKeys[e.RowIndex].Value);
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Users WHERE UserID = @UserID";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@UserID", userId);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+
+            LoadUsers();
+        }
+
+        //Add New Beneficiary
+        protected void btnAddBeneficiary_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Beneficiary (Name, AmountReceived) VALUES (@Name, @AmountReceived)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Name", txtBeneficiaryName.Text);
+                cmd.Parameters.AddWithValue("@AmountReceived", Convert.ToDecimal(txtAmountReceived.Text));
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+
+            LoadBeneficiaries();
+        }
+
+        //Delete Beneficiary
+        protected void gvBeneficiaries_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int beneficiaryId = Convert.ToInt32(gvBeneficiaries.DataKeys[e.RowIndex].Value);
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Beneficiary WHERE BeneficiaryID = @BeneficiaryID";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@BeneficiaryID", beneficiaryId);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+
+            LoadBeneficiaries();
+        }
     }
 }
